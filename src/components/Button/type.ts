1acd934cf4a24c467847eb415f252b13ffd08458
Gurusewak Kalra <gkalra@spire.io>
2024-02-26 22:33:00 -0800

    added comments for properties in typedoc

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,11 +1,21 @@
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./Button";
+/**
+ * This will be displayed as an interface
+ * @property a comment for a
+ * @prop b comment for b
+ * @interface
+ */
+
 
 export interface ButtonPropsType
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-  label: string;
+    /**this is test comment for description */
+  asChild?: boolean,
+  label: string,
+  variant: any,
+  size: any,
   onClick: () => void;
 }
 
